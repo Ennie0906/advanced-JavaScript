5ea632e29f80afafb2a846b84325490fd4f63ff6
Enes Arici <enesarici12@outlook.com>
2023-11-01 13:59:15 +0000

    finished object tutorial

--- script.js
+++ script.js
@@ -9,5 +9,18 @@ let person = {
     name: 'Enes', 
     age:21
 }
+ //Dot Notation 
+/* How to change the objects with a dot. 
+declare the object first then put a dot with value you want to change
+*/
+ person.name = 'John'
+person.age = '22'
 
+
+// Bracket Notation
+let selection = 'name'
+person[selection] = 'Mary'
+
+ console.log(person.name)
+ console.log(person.age)
 
