94667c1a7353df1465b106b82379f67e39738cb8
Enes Arici <enesarici12@outlook.com>
2023-11-01 13:26:10 +0000

    started objects tutorial

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+let isApproved = true; // Boolean Literal (true; as well)
+let firstName = undefined;
+let selectedColor = null; // Clear the value of a variablegi
+
+let person = {
+    name: 'Enes', 
+    age:21
+}
+
+
