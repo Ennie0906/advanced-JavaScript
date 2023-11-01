bc25602e1042c6016a480608146d30ef86de3281
Enes Arici <enesarici12@outlook.com>
2023-11-01 16:01:12 +0000

    functions tutorial complete

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
 
+
+
+//Performing a task
+
+
 /*This greet function has a paramater of name
 which is only meaningful in the greet function 
 */
@@ -7,6 +12,19 @@ function greet(name, lastName) {
     // Functions can have inputs
     console.log('Hello' + ' ' + name + ' ' + lastName)
 }
+
+//Calculating a value
+function square(number) {
+   return number * number
+}
+
+// putting the function in a variable
+/* dont forget the number in the paramater and the body of square function is the same
+number as the square function in the number variable
+*/
+let number = square(2);
+console.log(number)
+
 
 //Need to pass a value to the greet 
 //John is an argument to the greet function
